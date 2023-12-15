19c5f0fbfc2ec49b8876a7e41db64d3f1d139568
cesrra <c3z4r.rincon@gmail.com>
2023-12-15 17:23:41 -0500

    Implemented the layout in the Login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,37 @@
+import { Button, Grid, TextField } from "@mui/material"
+import { AuthLayout } from "../layout/AuthLayout"
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <AuthLayout title="Iniciar Sesión">
+      <form>
+        <Grid container >
+          <Grid item xs={12} sx={{ mt: 2 }} >
+            <TextField 
+              label="Usuario"
+              type="ematextil"
+              placeholder="Nombre de Usuario"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 3 }} >
+            <TextField 
+              label="Contraceña"
+              type="password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={ 2 } sx={{ mt: 1 }}>
+            <Grid item xs={ 12 } sm={ 12 }>
+              <Button variant='contained' fullWidth>
+                Iniciar
+              </Button>
+            </Grid>
+          </Grid>
+
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
