5dd0c39a603b391b00ba032da9b162f5f426fc65
cesrra <c3z4r.rincon@gmail.com>
2023-12-15 17:38:15 -0500

    Created an implementation to JournalLayout and the NavBar component

---
+++ src/ui/NavBar.jsx
@@ -0,0 +1,36 @@
+import { LogoutOutlined, MenuOutlined } from "@mui/icons-material"
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material"
+
+// eslint-disable-next-line react/prop-types
+export const NavBar = ({ drawerWidth = 0 }) => {
+  return (
+    <AppBar
+        position="fixed"
+        sx={{
+            width: { sm: `calc(100% - ${ drawerWidth }px)` },
+            ml: { sm: `${ drawerWidth }px`}
+        }}    
+    >
+        <Toolbar>
+            <IconButton
+                color="inherit"
+                edge="start"
+                sx={{ mr: 2, display: { sm: 'none' },  }}
+            >
+                <MenuOutlined />
+            </IconButton>
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Typography variant="h6" noWrap component="div" >FrutApp</Typography>
+                <IconButton color="error">
+                    <LogoutOutlined />
+                </IconButton>
+            </Grid>
+        </Toolbar>
+    </AppBar>
+  )
+}
