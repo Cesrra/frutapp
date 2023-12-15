051281c79fa41e65b840badedf845c97587d2918
cesrra <c3z4r.rincon@gmail.com>
2023-12-15 12:23:01 -0500

    Initial changes for setting up React with Vite configuration.

---
+++ src/Frutapp.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+
+function Frutapp() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="">Hola</div>
+    </>
+  )
+}
+
+export default Frutapp
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Frutapp from './Frutapp.jsx'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Frutapp />
+  </React.StrictMode>,
+)
