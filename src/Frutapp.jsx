99ec66a8b3df9fca94001577c55561053356791e
cesrra <c3z4r.rincon@gmail.com>
2023-12-18 08:21:48 -0500

    The context, provider, and reducer were created to manage user authentication

--- src/Frutapp.jsx
+++ src/Frutapp.jsx
@@ -1,13 +1,16 @@
 
+import { AuthProvider } from "./auth"
 import { AppRouter } from "./router/AppRouter"
 import { AppTheme } from "./theme/AppTheme"
 
 
 function Frutapp() {
   return (
-    <AppTheme>
-      <AppRouter />
-    </AppTheme>
+    <AuthProvider>
+      <AppTheme>
+        <AppRouter />
+      </AppTheme>
+    </AuthProvider>
   )
 }
 
